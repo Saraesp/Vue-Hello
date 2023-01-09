7a1561e083a329748253c75d527f8b63b33650f9
Saraesp <saraesposito145@gmail.com>
2023-01-09 19:38:21 +0100

    bonus image

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const{
 createApp({
     data(){
         return{
-            message: 'Hello word'
+            message: 'Hello word',
+            url: 'https://picsum.photos/id/109/4287/2392'
         }
     }
 }).mount('#app')
